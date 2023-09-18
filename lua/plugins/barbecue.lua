5c192f8d96376cdf5b169aac552fbea6526322f0
wixaxis <wixaxis@gmail.com>
2023-09-19 00:43:10 +0200

    lazy load barbecue

--- lua/plugins/barbecue.lua
+++ lua/plugins/barbecue.lua
@@ -6,7 +6,6 @@ return {
     "SmiteshP/nvim-navic",
     "nvim-tree/nvim-web-devicons", -- optional dependency
   },
-  opts = {
-    -- configurations go here
-  },
+  opts = {},
+  event = 'BufEnter',
 }
