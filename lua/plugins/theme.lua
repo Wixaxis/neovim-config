38711cd0a00e8a28fe3a15b871c0f700282f8af0
wixaxis <wixaxis@gmail.com>
2023-09-19 00:46:22 +0200

    fix slow loading of themes

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -9,12 +9,10 @@ end
 return {
   {
     'navarasu/onedark.nvim',
-    priority = 1000,
     config = set_theme('onedark'),
   },
   {
     'rmehri01/onenord.nvim',
-    priority = 1000,
     config = set_theme('onenord'),
   },
 }
