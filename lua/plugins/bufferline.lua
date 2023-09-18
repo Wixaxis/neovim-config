34315278036ab34e4345d7495c046a44d7e99e2c
wixaxis <wixaxis@gmail.com>
2023-09-18 21:14:26 +0200

    offset bufferline for nvimtree opened

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -3,5 +3,16 @@ return {
   version = "*",
   dependencies = 'nvim-tree/nvim-web-devicons',
   enabled = require 'configs.plugs'.bufferline,
-  config = true,
+  opts = {
+    options = {
+      offsets = {
+        {
+          filetype = "NvimTree",
+          text = "Nvim Tree",
+          separator = true,
+          text_align = "left"
+        }
+      },
+    }
+  },
 }
