652b0a20bd27f40091d1f84a2e587c599d6e24b3
wixaxis <wixaxis@gmail.com>
2023-09-19 00:46:48 +0200

    rounded mason window

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,9 +1,12 @@
 return {
   'neovim/nvim-lspconfig',
   dependencies = {
-    { 'williamboman/mason.nvim', config = true },
-    { 'j-hui/fidget.nvim',       tag = 'legacy', opts = {} },
-    { 'folke/neodev.nvim',       config = true },
+    {
+      'williamboman/mason.nvim',
+      opts = { ui = { border = 'rounded' } },
+    },
+    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
+    { 'folke/neodev.nvim', config = true, ft = 'lua' },
     'williamboman/mason-lspconfig.nvim',
   },
   enabled = require 'configs.plugs'.nvim_lspconfig
