d76b30c610477fb36ce358738600f89509c3da5f
wixaxis <wixaxis@gmail.com>
2023-09-19 22:02:57 +0200

    require neovide config + formatting

--- init.lua
+++ init.lua
@@ -3,9 +3,12 @@ require 'scripts.ensure-lazy'
 require('lazy').setup({ { import = 'plugins' } }, {})
 require 'configs.mappings'
 pcall(require('telescope').load_extension, 'fzf')
-if require'configs.plugs'.scope then
-require 'telescope'.load_extension 'scope'
+if require 'configs.plugs'.scope then
+	require 'telescope'.load_extension 'scope'
 end
 require 'configs.nvim-treesitter'
 require 'configs.mason-lspconfig'
 require 'configs.cmp'
+if vim.g.neovide then
+	require 'configs.neovide'
+end
