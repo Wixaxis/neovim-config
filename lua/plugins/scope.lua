70f9aa0bd643aa2135ebb3398389eceb0ea92e4b
wixaxis <wixaxis@gmail.com>
2023-09-20 02:44:56 +0200

    register scope telescope integration in lazy setup

--- lua/plugins/scope.lua
+++ lua/plugins/scope.lua
@@ -1,5 +1,9 @@
 return {
   "tiagovla/scope.nvim",
   enabled = require 'configs.plugs'.scope,
-  config = true,
+  dependencies = { 'nvim-telescope/telescope.nvim' },
+  config = function()
+    require 'scope'.setup {}
+    require 'telescope'.load_extension 'scope'
+  end,
 }
