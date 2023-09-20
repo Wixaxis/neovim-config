fc0417285f518bf6ebed7a2de6a5c754783a3f9f
wixaxis <wixaxis@gmail.com>
2023-09-20 02:42:54 +0200

    fix missing coma

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -1,6 +1,6 @@
 return {
   { 'kchmck/vim-coffee-script', ft = 'coffee' },
-  { 'slim-template/vim-slim',   ft = { 'slim', 'html.slim' } }
-  { "vim-test/vim-test", ft = 'rb' },
+  { 'slim-template/vim-slim',   ft = { 'slim', 'html.slim' } },
+  { "vim-test/vim-test", ft = '' },
 
 }
