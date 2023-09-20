a5769a821da6420ea5fb16fb6cf23f2536620171
wixaxis <wixaxis@gmail.com>
2023-09-20 02:45:14 +0200

    reformat init, neovide options first

--- init.lua
+++ init.lua
@@ -1,14 +1,11 @@
 require 'configs.core'
+if vim.g.neovide then
+	require 'configs.neovide'
+end
 require 'scripts.ensure-lazy'
 require('lazy').setup({ { import = 'plugins' } }, {})
 require 'configs.mappings'
 pcall(require('telescope').load_extension, 'fzf')
-if require 'configs.plugs'.scope then
-	require 'telescope'.load_extension 'scope'
-end
 require 'configs.nvim-treesitter'
 require 'configs.mason-lspconfig'
 require 'configs.cmp'
-if vim.g.neovide then
-	require 'configs.neovide'
-end
