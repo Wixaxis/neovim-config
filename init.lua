1ea2712020ac31570827ec6b983a656684e05575
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2023-09-22 02:05:53 +0200

    formatting of init.lua

--- init.lua
+++ init.lua
@@ -1,11 +1,9 @@
 require 'configs.core'
-if vim.g.neovide then
-	require 'configs.neovide'
-end
 require 'scripts.ensure-lazy'
 require('lazy').setup({ { import = 'plugins' } }, {})
+if vim.g.neovide then require 'configs.neovide' end
 require 'configs.mappings'
-pcall(require('telescope').load_extension, 'fzf')
+pcall(require 'telescope'.load_extension, 'fzf')
 require 'configs.nvim-treesitter'
 require 'configs.mason-lspconfig'
 require 'configs.cmp'
