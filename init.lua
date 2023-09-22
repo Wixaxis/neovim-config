1516f13da430022f4bcebdd0443236225f5914b3
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2023-09-22 03:45:05 +0200

    make neovide config rerunnable function

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@ require 'configs.core'
 require 'scripts.ensure-lazy'
 require('lazy').setup({ { import = 'plugins' } }, {})
 require 'configs.mappings'
-if vim.g.neovide then require 'configs.neovide' end
+if vim.g.neovide then require 'configs.neovide'.set_defaults() end
 pcall(require 'telescope'.load_extension, 'fzf')
 require 'configs.nvim-treesitter'
 require 'configs.mason-lspconfig'
--- lua/configs/neovide.lua
+++ lua/configs/neovide.lua
@@ -1,21 +1,29 @@
 local system = vim.loop.os_uname().sysname
 
 -- TODO: Make it re-runnable function, to be called as reset live changes
-if system == 'Linux' then
-  vim.o.guifont = "FiraCode Nerd Font Mono:h9"
-  vim.g.neovide_scale_factor = 0.7
-  vim.g.neovide_transparency = 0.9
-  vim.g.transparency = 1
-  vim.notify 'Loaded Linux Neovide config'
-elseif system == 'Darwin' then
-  vim.o.guifont = "FiraCode Nerd Font Mono:h14"
-  vim.g.neovide_scale_factor = 0.9
-  vim.g.neovide_input_macos_alt_is_meta = true -- TODO: doesn't work :/
-  vim.g.neovide_transparency = 0
-  vim.g.transparency = 0.9
-  local bg_color = string.sub(vim.api.nvim_cmd({ cmd = 'hi', args = { 'normal' } }, { output = true }), -7, -1)
-  vim.g.neovide_background_color = bg_color .. string.format("%x", math.floor((255 * (vim.g.transparency or 0.8))))
-  vim.notify 'Loaded MacOS Neovide config'
+local M = {}
+
+M.set_defaults = function()
+  if system == 'Linux' then
+    vim.o.guifont = "FiraCode Nerd Font Mono:h9"
+    vim.g.neovide_scale_factor = 0.7
+    vim.g.neovide_transparency = 0.9
+    vim.g.transparency = 1
+    vim.notify 'Loaded Linux Neovide config'
+  elseif system == 'Darwin' then
+    vim.o.guifont = "FiraCode Nerd Font Mono:h14"
+    vim.g.neovide_scale_factor = 0.9
+    vim.g.neovide_input_macos_alt_is_meta = true
+    vim.g.neovide_transparency = 0
+    vim.g.transparency = 0.9
+    local bg_color = string.sub(vim.api.nvim_cmd({ cmd = 'hi', args = { 'normal' } }, { output = true }), -7, -1)
+    vim.g.neovide_background_color = bg_color .. string.format("%x", math.floor((255 * (vim.g.transparency or 0.8))))
+    vim.notify 'Loaded MacOS Neovide config'
+  end
 end
+
+
+
+return M
 
 -- TODO: make keymaps for changing scale, font-size, transparency, reload_color_from_theme
