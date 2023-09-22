f5b955b2e6d607f9e48e70e77752f5c3ce0f497d
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2023-09-22 02:05:16 +0200

    neovide config improvements

--- lua/configs/neovide.lua
+++ lua/configs/neovide.lua
@@ -1,18 +1,22 @@
-vim.o.guifont = "FiraCode Nerd Font Mono:h9"
-vim.g.neovide_scale_factor = 0.7
-
-vim.g.neovide_input_macos_alt_is_meta = false
-local alpha = function()
-  return string.format("%x", math.floor((255 * vim.g.transparency) or 0.8))
-end
-
 local system = vim.loop.os_uname().sysname
 
+-- TODO: Make it re-runnable function, to be called as reset live changes
 if system == 'Linux' then
+  vim.o.guifont = "FiraCode Nerd Font Mono:h9"
+  vim.g.neovide_scale_factor = 0.7
   vim.g.neovide_transparency = 0.9
   vim.g.transparency = 1
+  vim.notify 'Loaded Linux Neovide config'
 elseif system == 'Darwin' then
-  vim.g.neovide_transparency = 0.0
-  vim.g.transparency = 0.8
-  vim.g.neovide_background_color = "#0f1117" .. alpha()
+  vim.o.guifont = "FiraCode Nerd Font Mono:h14"
+  vim.g.neovide_scale_factor = 0.9
+  vim.g.neovide_input_macos_alt_is_meta = true -- TODO: doesn't work :/
+  vim.g.neovide_transparency = 0.9
+  vim.g.transparency = 0.9
+  -- vim.g.neovide_background_color = "#000000" .. string.format("%x", math.floor((255 * (vim.g.transparency or 0.8))))
+  -- TODO: read current background color from current theme
+  vim.g.neovide_background_color = "#000000" .. string.format("%x", math.floor(255 * 0.1))
+  vim.notify 'Loaded MacOS Neovide config'
 end
+
+-- TODO: make keymaps for changing scale, font-size, transparency, reload_color_from_theme
