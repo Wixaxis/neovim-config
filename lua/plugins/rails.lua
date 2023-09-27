1edf6b37ed1e60b13e1e512f6d60a9c6a56d0cc1
wixaxis <wixaxis@gmail.com>
2023-09-27 21:39:10 +0200

    formatting

--- lua/plugins/rails.lua
+++ lua/plugins/rails.lua
@@ -2,5 +2,4 @@ return {
   { 'kchmck/vim-coffee-script', ft = 'coffee' },
   { 'slim-template/vim-slim',   ft = { 'slim', 'html.slim' } },
   { "vim-test/vim-test", ft = '' }, -- NOTE: Not really rails exclusive, maybe move to general plugins?
-
 }
--- lua/utils.lua
+++ lua/utils.lua
@@ -10,38 +10,38 @@ M.ensure_lazy_installed = function()
 	vim.opt.rtp:prepend(lazypath)
 end
 
-M.cmp = {
-	confirm_mapping = function(cmp)
-		return cmp.mapping.confirm {
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = true,
-		}
-	end,
+M.cmp = {}
 
-	next_item = function(cmp, luasnip)
-		return cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_next_item()
-			elseif luasnip.expand_or_locally_jumpable() then
-				luasnip.expand_or_jump()
-			else
-				fallback()
-			end
-		end, { 'i', 's' })
-	end,
+M.cmp.confirm_mapping = function(cmp)
+	return cmp.mapping.confirm {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = true,
+	}
+end
 
-	prev_item = function(cmp, luasnip)
-		return cmp.mapping(function(fallback)
-			if cmp.visible() then
-				cmp.select_prev_item()
-			elseif luasnip.locally_jumpable(-1) then
-				luasnip.jump(-1)
-			else
-				fallback()
-			end
-		end, { 'i', 's' })
-	end,
-}
+M.cmp.next_item = function(cmp, luasnip)
+	return cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_next_item()
+		elseif luasnip.expand_or_locally_jumpable() then
+			luasnip.expand_or_jump()
+		else
+			fallback()
+		end
+	end, { 'i', 's' })
+end
+
+M.cmp.prev_item = function(cmp, luasnip)
+	return cmp.mapping(function(fallback)
+		if cmp.visible() then
+			cmp.select_prev_item()
+		elseif luasnip.locally_jumpable(-1) then
+			luasnip.jump(-1)
+		else
+			fallback()
+		end
+	end, { 'i', 's' })
+end
 
 M.infile_search = function()
 	require 'telescope.builtin'.current_buffer_fuzzy_find(require 'telescope.themes'.get_dropdown {
