1f10e5ed29aee206cb43ed86a48ac7c04cc22baf
wixaxis <wixaxis@gmail.com>
2023-10-02 19:22:26 +0200

    make utils a package

--- lua/utils/init.lua
+++ lua/utils/init.lua
@@ -2,11 +2,9 @@ local M = {}
 
 M.ensure_lazy_installed = function()
 	local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-	if not vim.loop.fs_stat(lazypath) then
-		vim.fn.system { 'git', 'clone', '--filter=blob:none', 'https://github.com/folke/lazy.nvim.git',
-			'--branch=stable',
-			lazypath }
-	end
+	local install_script = { 'git', 'clone', '--filter=blob:none',
+		'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath }
+	if not vim.loop.fs_stat(lazypath) then vim.fn.system(install_script) end
 	vim.opt.rtp:prepend(lazypath)
 end
 
