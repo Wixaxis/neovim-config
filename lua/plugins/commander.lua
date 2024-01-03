a640a1d1059a25c7b49aff922573b18e7072acb8
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2024-01-03 05:37:31 +0100

    commander nvim enhancements

--- lua/plugins/commander.lua
+++ lua/plugins/commander.lua
@@ -1,4 +1,11 @@
 return {
   "FeiyouG/commander.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" }
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  opts = {
+    components = { 'KEYS', 'DESC', 'CAT', 'CMD' },
+    integration = {
+      telescope = { enable = true },
+      lazy = { enable = true }
+    }
+  }
 }
