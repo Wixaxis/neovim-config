0324c82e5e0e220d6293adbf0529bd3986774c47
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2024-01-30 23:43:42 +0100

    test suite command

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -3,5 +3,6 @@ return {
     ft = '',
     commander = {
         { cmd = ':TestFile -strategy=neovim_sticky\n', desc = 'Run current test file in sticky terminal' },
+        { cmd = ':TestSuite -strategy=neovim_sticky\n', desc = 'Run all tests in sticky terminal' },
     },
 }
