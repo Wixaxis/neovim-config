6bf8276fd421cec19d3666c0641efb4122a9c841
Wiktor Rojecki <wixaxis@gmail.com>
2024-04-19 00:08:18 +0200

    change default theme from nordic to onenord
    the nordic package for some reason doesnt affect the lazygit client

--- lua/defaults.lua
+++ lua/defaults.lua
@@ -1,5 +1,5 @@
 return {
-	theme = 'nordic',
+	theme = 'onenord',
 	neovide = {
 		-- every setting can be put both on system specific and general levels
 		-- system specific settings take precedence over general
