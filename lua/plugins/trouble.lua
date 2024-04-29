d4d304fe210312511cf20044697494a7e579dec4
Wiktor Rojecki <wixaxis@gmail.com>
2024-04-29 03:57:17 +0200

    add trouble.nvim plugin

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,7 @@
+return {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    -- TODO: Add commander commands for Trouble.nvim
+    -- commander = {},
+}
