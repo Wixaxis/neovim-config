2c5ceb7600cbc7bd57298ae827c9162e81c0eeac
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2024-06-04 13:45:40 +0200

    replacereplace neodev with lazydev

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -7,4 +7,11 @@ return {
     'rafamadriz/friendly-snippets',
   },
   event = 'BufEnter *.*',
+  opts = function(_, opts)
+    opts.sources = opts.sources or {}
+    table.insert(opts.sources, {
+      name = "lazydev",
+      group_index = 0,
+    })
+  end,
 }
--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -5,8 +5,13 @@ return {
       'williamboman/mason.nvim',
       opts = { ui = { border = 'rounded' } },
     },
-    { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
-    { 'folke/neodev.nvim', config = true, ft = 'lua' },
+    { 'j-hui/fidget.nvim',    opts = {} },
     'williamboman/mason-lspconfig.nvim',
+    {
+      "folke/lazydev.nvim",
+      ft = "lua",
+      opts = { library = { "luvit-meta/library" } },
+    },
+    { "Bilal2453/luvit-meta", lazy = true },
   },
 }
