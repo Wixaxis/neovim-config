9df884bdf8b9a9692e5b4f75ea174e42a49a26aa
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2025-01-20 08:16:50 +0100

    add better config for which key with helix preset and keymap for local buffer keymaps

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,4 +1,11 @@
 return {
-  'folke/which-key.nvim',
-  opts = {},
+    'folke/which-key.nvim',
+    opts = {
+        preset = 'helix',
+    },
+    commander = { {
+        keys = { 'n', '<leader>?', { desc = 'which-key - show local bufer keymaps' } },
+        cmd = function() require 'which-key'.show({ global = false }) end,
+        desc = 'which-key - show local bufer keymaps'
+    } }
 }
