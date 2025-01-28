128dc9717e51d80c9b0d0c02e14f50bf29a2a575
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2025-01-28 01:31:09 +0100

    add commentstring for slim

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -4,6 +4,7 @@ return {
     --- @diagnostic disable-next-line: missing-fields
     require 'Comment'.setup { mappings = false }
     require 'Comment.ft'.set('hyprlang', '#%s')
+    require 'Comment.ft'.set('slim', '/%s')
   end,
   lazy = true,
   commander = require 'configs.mappings'.comment,
