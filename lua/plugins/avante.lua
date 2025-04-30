f2bd817bbf300f4f953f346df2b5c4a190522193
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2025-04-30 17:42:25 +0200

    custom max tokens for providers

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -2,9 +2,22 @@ return {
     "yetone/avante.nvim",
     lazy = false,
     version = false,
-    opts = {},
+    opts = {
+        provider = "claude",
+        cursor_applying_provider = 'gemini',
+        behaviour = {
+            enable_cursor_planning_mode = false,
+        },
+        claude = {
+            max_tokens = 4096,
+        },
+        gemini = {
+            max_tokens = 32768,
+        }
+    },
     build = "make",
     dependencies = {
+        "nvim-treesitter/nvim-treesitter",
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
@@ -16,13 +29,13 @@ return {
                 default = {
                     embed_image_as_base64 = false,
                     prompt_for_file_name = false,
-                    drag_and_drop = { insert_mode = true, },
+                    drag_and_drop = { insert_mode = true },
                 },
             },
         },
         {
             'MeanderingProgrammer/render-markdown.nvim',
-            opts = { file_types = { "markdown", "Avante" }, },
+            opts = { file_types = { "markdown", "Avante" } },
             ft = { "markdown", "Avante" },
         },
     },
