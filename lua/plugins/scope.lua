a4d0e329f1bea5846b06e31986de63bbb9124b56
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2025-06-18 06:55:41 +0200

    fix scope nvim

--- lua/plugins/scope.lua
+++ lua/plugins/scope.lua
@@ -1,8 +1,4 @@
 return {
   "tiagovla/scope.nvim",
-  dependencies = { 'nvim-telescope/telescope.nvim' },
-  config = function()
-    require 'scope'.setup {}
-    require 'telescope'.load_extension 'scope'
-  end,
+  opts = true,
 }
