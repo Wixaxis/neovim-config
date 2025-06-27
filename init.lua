e285ccacc25a69590636f685875efa69005e7814
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2025-06-28 00:44:28 +0200

    add lsp progress notification to init.lua

--- init.lua
+++ init.lua
@@ -9,6 +9,22 @@ require 'configs.mappings'.assign_base_mappings()
 if vim.g.neovide then require 'configs.neovide' end
 require 'configs.nvim-treesitter'
 
+vim.api.nvim_create_autocmd('LspProgress', {
+  ---@param ev {data: {client_id: integer, params: lsp.ProgressParams}}
+  callback = function(ev)
+    local spinner = { '⠋', '⠙', '⠹', '⠸', '⠼', '⠴', '⠦', '⠧', '⠇', '⠏' }
+    vim.notify(vim.lsp.status(), 'info', {
+      id = 'lsp_progress',
+      title = 'LSP Progress',
+      opts = function(notif)
+        notif.style = 'minimal'
+        notif.icon = ev.data.params.value.kind == 'end' and ' ' or
+            spinner[math.floor(vim.uv.hrtime() / (1e6 * 80)) % #spinner + 1]
+      end,
+    })
+  end,
+})
+
 -- TODO: Move all below to some config file
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
