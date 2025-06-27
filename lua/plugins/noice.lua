50499775496ae3de30923ae86dc5d4d162aa608c
Wiktor Rojecki <44247677+Wixaxis@users.noreply.github.com>
2025-06-28 00:51:23 +0200

    noice without nonsense

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -1,7 +1,16 @@
 -- Provides better UI for messages, command line, and notifications
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    opts = { notify = { enabled = false } },
-    dependencies = { "MunifTanjim/nui.nvim" }
+  'folke/noice.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- messages = { enabled = false },
+    notify = { enabled = false },
+    lsp = {
+      progress = { enabled = false },
+      hover = { enabled = false },
+      signature = { enabled = false },
+      message = { enabled = false },
+    },
+  },
+  dependencies = { 'MunifTanjim/nui.nvim' },
 }
